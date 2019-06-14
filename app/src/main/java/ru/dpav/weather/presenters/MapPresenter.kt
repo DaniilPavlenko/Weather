fd492eaa761ca0aad006aff4b1143548af4e082d
Daniil Pavlenko <pavlenko.develop@gmail.com>
2019-06-14 11:09:54 +0300

    Added MOXY and used in MapFragment

---
+++ app/src/main/java/ru/dpav/weather/presenters/MapPresenter.kt
@@ -0,0 +1,124 @@
+package ru.dpav.weather.presenters
+
+import android.content.Context
+import com.arellomobile.mvp.InjectViewState
+import com.arellomobile.mvp.MvpPresenter
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import ru.dpav.weather.R
+import ru.dpav.weather.api.City
+import ru.dpav.weather.api.WeatherApi
+import ru.dpav.weather.api.WeatherResponse
+import ru.dpav.weather.views.MapView
+import java.io.IOException
+
+@InjectViewState
+class MapPresenter : MvpPresenter<MapView>() {
+	private var mCities: List<City> = ArrayList()
+	private var isFirstCreate = true
+	private lateinit var mContext: Context
+
+	fun setContext(context: Context) {
+		mContext = context
+	}
+
+	fun onMapClick(latLng: LatLng) {
+		viewState.setMapMarker(latLng)
+		getWeather(latLng)
+	}
+
+	fun onCameraMoveTo(latLng: LatLng, zoom: Float) {
+		viewState.moveCameraTo(latLng, zoom)
+	}
+
+	private fun getWeather(latLng: LatLng) {
+		viewState.showUpdateScreen()
+		WeatherApi.getInstance().getWeatherByCoordinates(mContext, latLng,
+			object : Callback<WeatherResponse> {
+				override fun onFailure(call: Call<WeatherResponse>, t: Throwable) {
+					if (t is IOException) {
+						viewState.showConnectionError(latLng)
+					}
+				}
+
+				override fun onResponse(call: Call<WeatherResponse>, response: Response<WeatherResponse>) {
+					processApiResponse(response)
+				}
+			})
+	}
+
+	fun processApiResponse(response: Response<WeatherResponse>) {
+		val cities = response.body()?.cities
+		if (cities == null) {
+			response.body()?.message?.let { viewState.showToastError(it) }
+			return
+		}
+		if (mCities == cities) {
+			viewState.hideUpdateScreen()
+			return
+		}
+		mCities = cities
+		viewState.updateMapMarkers(cities)
+		viewState.hideUpdateScreen()
+	}
+
+	fun onMarkerClick(marker: Marker) {
+		viewState.openInfoWindow(marker)
+	}
+
+	fun onInfoWindowClick(marker: Marker) {
+		for (i in 0 until mCities.size) {
+			if (mCities[i].name == marker.title) {
+				viewState.showDetailInfo(mCities[i])
+			}
+		}
+	}
+
+	fun onLocationEnable() {
+		viewState.enableLocation()
+	}
+
+	fun onLocationDisable() {
+		viewState.disableLocation()
+	}
+
+	fun onLocationIsDisabled() {
+		viewState.showLocationIsDisabled()
+	}
+
+	fun onServicesAvailable() {
+		viewState.showUpdateScreen()
+	}
+
+	fun onServicesUnavailable() {
+		viewState.hideUpdateScreen()
+	}
+
+	fun onMapReady() {
+		viewState.hideUpdateScreen()
+		if (isFirstCreate) {
+			isFirstCreate = false
+			viewState.moveToStartPosition()
+		}
+	}
+
+	fun onMoveToDefaultPosition() {
+		val defaultLatLng = LatLng(
+			mContext.getString(R.string.defaultLatitude).toDouble(),
+			mContext.getString(R.string.defaultLongitude).toDouble())
+		viewState.moveToDefaultPosition(defaultLatLng)
+	}
+
+	fun onInfoWindowClose() {
+		viewState.closeInfoWindow()
+	}
+
+	fun onAskPermission() {
+		if (isFirstCreate) {
+			viewState.askPermission()
+		}
+	}
+}
---
+++ app/src/main/java/ru/dpav/weather/views/MapView.kt
@@ -0,0 +1,55 @@
+package ru.dpav.weather.views
+
+import com.arellomobile.mvp.MvpView
+import com.arellomobile.mvp.viewstate.strategy.AddToEndSingleStrategy
+import com.arellomobile.mvp.viewstate.strategy.OneExecutionStateStrategy
+import com.arellomobile.mvp.viewstate.strategy.StateStrategyType
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
+import ru.dpav.weather.api.City
+import ru.dpav.weather.strategies.AddToEndSingleByTagStateStrategy
+
+@StateStrategyType(OneExecutionStateStrategy::class)
+interface MapView : MvpView {
+
+	@StateStrategyType(AddToEndSingleStrategy::class)
+	fun setMapMarker(latLng: LatLng)
+
+	@StateStrategyType(AddToEndSingleStrategy::class)
+	fun updateMapMarkers(cities: List<City>)
+
+	@StateStrategyType(value = AddToEndSingleByTagStateStrategy::class, tag = TAG_INFO_WINDOW)
+	fun openInfoWindow(marker: Marker)
+
+	@StateStrategyType(value = AddToEndSingleByTagStateStrategy::class, tag = TAG_INFO_WINDOW)
+	fun closeInfoWindow()
+
+	fun showDetailInfo(city: City)
+
+	fun moveCameraTo(latLng: LatLng, zoom: Float?)
+
+	fun moveToStartPosition()
+
+	fun moveToDefaultPosition(latLng: LatLng)
+
+	fun enableLocation()
+
+	fun disableLocation()
+
+	fun showUpdateScreen()
+
+	fun hideUpdateScreen()
+
+	fun showConnectionError(latLng: LatLng)
+
+	fun showToastError(message: String)
+
+	fun showLocationIsDisabled()
+
+	@StateStrategyType(OneExecutionStateStrategy::class)
+	fun askPermission()
+
+	companion object {
+		const val TAG_INFO_WINDOW: String = "tagInfoWindow"
+	}
+}
