0d4701e3731617900b30fda2e7991dab582d2b95
Daniil Pavlenko <pavlenko.develop@gmail.com>
2024-05-15 13:18:26 +0300

    #14 Remove useless default field value for 'id' in City

--- app/src/main/java/ru/dpav/weather/data/api/model/City.kt
+++ app/src/main/java/ru/dpav/weather/data/api/model/City.kt
@@ -4,7 +4,7 @@ import com.google.gson.annotations.SerializedName
 
 data class City(
     @SerializedName("id")
-    val id: Int = 0,
+    val id: Int,
     @SerializedName("name")
     val name: String,
     @SerializedName("coord")
