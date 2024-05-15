5d56f463af5a9caad2b44e093e8512a1c182e7f6
Daniil Pavlenko <pavlenko.develop@gmail.com>
2024-05-15 18:30:54 +0300

    #2 Bump up `targetSdk` to 34

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
     defaultConfig {
         applicationId = "ru.dpav.weather"
         minSdk = 26
-        targetSdk = 31
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
