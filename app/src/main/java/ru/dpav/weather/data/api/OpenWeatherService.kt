f003ed009cbbb5f5b56900775d7bd1808b074d75
Daniil Pavlenko <pavlenko.develop@gmail.com>
2024-05-15 14:18:18 +0300

    #13 Specify query parameters as parameters with default values

--- app/src/main/java/ru/dpav/weather/data/api/OpenWeatherService.kt
+++ app/src/main/java/ru/dpav/weather/data/api/OpenWeatherService.kt
@@ -5,14 +5,18 @@ import retrofit2.http.Query
 import ru.dpav.weather.data.api.model.WeatherResponse
 
 interface OpenWeatherService {
-    @GET("find/?cluster=yes&units=metric&cnt=$CITIES_COUNT&lang=$LANGUAGE")
+    @GET("find/")
     suspend fun getWeather(
         @Query("lat") latitude: Double,
         @Query("lon") longitude: Double,
+        @Query("cluster") cluster: String = "yes",
+        @Query("units") units: String = "metric",
+        @Query("cnt") count: Int = MAX_CITIES_COUNT_PER_REQUEST,
+        @Query("lang") language: String = LANGUAGE,
     ): WeatherResponse
 
     companion object {
-        const val CITIES_COUNT = 50
-        const val LANGUAGE = "es"
+        const val MAX_CITIES_COUNT_PER_REQUEST = 50
+        const val LANGUAGE = "en"
     }
 }
