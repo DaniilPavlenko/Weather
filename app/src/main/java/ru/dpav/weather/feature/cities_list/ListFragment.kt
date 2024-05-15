836d59401b7f9f60e9b5fa59e24322c3d98becf0
Daniil Pavlenko <pavlenko.develop@gmail.com>
2024-05-15 13:12:21 +0300

    #17 #18 Remove useless onCreateView call & binding field

--- app/src/main/java/ru/dpav/weather/feature/cities_list/ListFragment.kt
+++ app/src/main/java/ru/dpav/weather/feature/cities_list/ListFragment.kt
@@ -1,9 +1,7 @@
 package ru.dpav.weather.feature.cities_list
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.commit
@@ -18,19 +16,12 @@ class ListFragment : Fragment(R.layout.fragment_list) {
 
     private val viewModel by viewModels<ListViewModel>()
 
-    private var binding: FragmentListBinding? = null
     private var listAdapter: CitiesAdapter? = null
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?,
-    ): View? = inflater.inflate(R.layout.fragment_list, container, false)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         listAdapter = CitiesAdapter(::navigateToDetails)
-        binding = FragmentListBinding.bind(view).apply {
+        FragmentListBinding.bind(view).apply {
             citiesRecyclerView.run {
                 setHasFixedSize(true)
                 layoutManager = LinearLayoutManager(requireContext())
@@ -48,7 +39,6 @@ class ListFragment : Fragment(R.layout.fragment_list) {
 
     override fun onDestroyView() {
         super.onDestroyView()
-        binding = null
         listAdapter = null
     }
 
