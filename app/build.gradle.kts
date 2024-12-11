2a9af29abfd032a888ac8783e36248dbb80331c0
Daniil Pavlenko <pavlenko.develop@gmail.com>
2024-12-11 18:22:06 +0300

    v1.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,8 +9,8 @@ android {
 
     defaultConfig {
         applicationId = "ru.dpav.weather"
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.1.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
     }
